4e83781a62a23cbc39fe9acff4047477ae80281d
zzzzzkjs <zkjs@naver.com>
2021-01-14 17:20:55 +0900

    플라스크 백엔드 설정 완료

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from .main.controller.fileController import api as file_ns
 from .main.controller.deepLearningController import api as dl_ns
 from .main.controller.user_controller import api as user_ns
 from .main.controller.auth_controller import api as auth_ns
+from .main.controller.view_controller import api as view_ns
 # -> 각 controller에서 만든 api 함수를 가져와서 아래 blueprint에 등록해서 사용가능하도록
 
 blueprint = Blueprint('api', __name__)
@@ -20,3 +21,4 @@ api.add_namespace(file_ns, path='/file')
 api.add_namespace(dl_ns, path='/dl')
 api.add_namespace(user_ns, path='/user')
 api.add_namespace(auth_ns)
+api.add_namespace(view_ns)
--- app/main/__init__.py
+++ app/main/__init__.py
@@ -4,13 +4,19 @@ from flask_bcrypt import Bcrypt
 
 from .config import config_by_name
 from flask.app import Flask
+import os
 
 db = SQLAlchemy()
 flask_bcrypt = Bcrypt()
 
+# view관련 파일들 경로 설정
+APP_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_PATH = os.path.join(APP_PATH, '../templates/')
+STATIC_PATH = os.path.join(APP_PATH, '../static/')
 
 def create_app(config_name: str) -> Flask:
-    app = Flask(__name__)
+    app = Flask(__name__,template_folder=TEMPLATE_PATH, static_folder=STATIC_PATH)
+    
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
